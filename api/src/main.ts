f8e3f483b1d24c21b20b1d30384815e9c9b6effc
mansouris2020 <mansouris2020@gmail.com>
2025-04-28 02:37:07 +0000

    authentification client api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('ISIM API')
+    .setDescription('The ISIM API description')
+    .setVersion('1.0')
+    .addTag('isim')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`App is running on http://localhost:${port}/api`)
 }
 bootstrap();
