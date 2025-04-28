b1779373b81084738b93655f13ef840c23f7e193
mansouris2020 <mansouris2020@gmail.com>
2025-04-28 23:00:19 +0000

    manage user

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,17 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { JwtModule } from "@nestjs/jwt";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule, JwtService } from "@nestjs/jwt";
 import { UserModule } from "src/user/user.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({ secret: "your-secret-key" }),
+    PassportModule,
+    JwtModule.register({ 
+      secret: "your-secret-key",
+      signOptions: { expiresIn: "1d" }
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService,JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule { }
