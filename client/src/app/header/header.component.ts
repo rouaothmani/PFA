11a6eceb2812dcb587e017e4d021558abbdd89cd
rouaothmanii <rouaothmanii@gmail.com>
2025-04-28 01:43:10 +0000

    header

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderService } from './header.service';
 
 @Component({
@@ -6,6 +6,21 @@ import { HeaderService } from './header.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
-     
+  cin: string = '';
+  password!: string;
+
+  constructor(private headerService: HeaderService) { }
+
+  submit() {
+    this.headerService.login(Number(this.cin), this.password).subscribe(
+      (response) => {
+        console.log('Login successful:', response);
+      },
+      (error) => {
+        console.error('Login failed:', error);
+      }
+    );
   }
+}
