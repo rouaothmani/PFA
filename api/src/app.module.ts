c69393cb426e94f3ab8f23c6654361a026404d36
roua othmani <rouaothmanioffic@gmail.com>
2025-04-29 01:59:59 +0100

    changes

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -11,7 +11,7 @@ import { UserModule } from './user/user.module';
         host: 'localhost',
         port: 5432,
         username: 'postgres',
-        password: 'postgres',
+        password: 'postgresql',
         database: 'agil',
         autoLoadEntities: true,
         synchronize: true,
