1248f6cf699be859235a7e8001f73862297773cc
mansouris2020 <mansouris2020@gmail.com>
2025-04-29 00:56:48 +0000

    logout

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { HeaderService } from './header.service';
@@ -12,30 +12,42 @@ interface user {
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  cin: number = 24681357;
-  password!: 'userPass1234';
+export class HeaderComponent implements OnInit {
+  cin: number = 12456778
+  password = "password"
+  isLoggedIn: boolean = false
+  userName: string = ''
+  constructor(private headerService: HeaderService, private toastrService: ToastrService, private router: Router) {
 
-  constructor(private headerService: HeaderService, private toastrService: ToastrService, private router: Router) {}
+  }
+  ngOnInit(): void {
+
+  }
+
+  logout() {
+    this.isLoggedIn = false
+    this.userName = ''
+    this.toastrService.success('Logout successful!')
+    this.router.navigate(['/'])
+  }
+
 
   submit() {
     this.headerService.login(this.cin, this.password).subscribe(
       (response) => {
-        console.log('Login successful:', response);
-        this.toastrService.success('Login Successful!');
-        this.router.navigate(['/profile']);
-        this.isLoggedIn = true;
+        console.log('Login successful:', response)
+        this.toastrService.success('Login Successful!')
+        this.router.navigate(['/profile'])
+        this.isLoggedIn = true
         this.headerService.getUserByCin(this.cin).subscribe((response: user) => {
-          this.userName = response.name;
+          this.userName = response.name
         })
 
       },
       (error) => {
-        console.error('Login failed:', error);
-        this.toastrService.error('Login Failed!');
+        console.error('Login failed:', error)
+        this.toastrService.error('Login Failed!')
       }
     );
   }
 }
-
-
